0513a8e957b29f42947d4c2097908b1631eddbdb
CarlesVicedo <vicedocarles@gmail.com>
2025-01-22 18:06:45 +0100

    Add validations to create budget endpoint

--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -1,10 +1,20 @@
 import { Router } from "express"
+import { body } from "express-validator"
 import { BudgetControler } from "../controllers/BudgetController"
+import { handleInputErrors } from "../middleware/validation"
 
 const router = Router()
 
 router.get('/', BudgetControler.getAll)
-router.post('/', BudgetControler.create)
+router.post('/',
+    body('name')
+        .notEmpty().withMessage('Budget name is mandatory'),
+    body('amount')
+        .notEmpty().withMessage('Budget amount is mandatory')
+        .isNumeric().withMessage('Not valid amount')
+        .custom(value => value > 0).withMessage('Budget must be higher than 0.'),
+    handleInputErrors,
+    BudgetControler.create)
 router.get('/:id', BudgetControler.getById)
 router.put('/:id', BudgetControler.updateById)
 router.delete('/:id', BudgetControler.deleteById)
