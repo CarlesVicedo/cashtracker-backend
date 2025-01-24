7f48b524bf8139780861f37e68cfb2aa6a9dfcd9
CarlesVicedo <vicedocarles@gmail.com>
2025-01-24 12:19:34 +0100

    Return an explainatory message when an email is already registered in the database

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,11 +3,20 @@ import User from "../models/User"
 
 export class AuthControler {
     static createAccount = async (req: Request, res: Response) => {
+        const { email } = req.body
+        
+        const userExists = await User.findOne({where: { email }})
+
+        if(userExists) {
+            const error = new Error('A user with this email already exists')
+            res.status(409).json({error: error.message})
+        }
+
         try {
             const user = new User(req.body)
             await user.save()
             res.json('Account created')
-            
+
         } catch (error) {
             res.status(500).json({error: 'Something broke'})
         }
