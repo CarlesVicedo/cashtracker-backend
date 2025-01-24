9832e3bd35b7df8f57ba33b58e8649d3caa5718e
CarlesVicedo <vicedocarles@gmail.com>
2025-01-24 11:58:15 +0100

    Create validations fot create-account endpoint

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import { AuthControler } from "../controllers/authController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
-router.post('/create-account', AuthControler.createAccount)
+router.post('/create-account',
+    body('name')
+        .notEmpty()
+        .withMessage('Name can not be empty'),
+    body('password')
+        .isLength({min: 8})
+        .withMessage('Password is too short. It must be at least 8 characters'),
+    body('email')
+        .isEmail()
+        .withMessage('Not valid email'),
+    handleInputErrors,
+    AuthControler.createAccount)
 
 export default router
