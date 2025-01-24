cc32910465eb532a31bdcb530ff1fed834b16d3c
CarlesVicedo <vicedocarles@gmail.com>
2025-01-24 12:26:52 +0100

    Hash user password before storing it in the DB

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,10 @@
 import type { Request, Response } from "express"
 import User from "../models/User"
+import { hashPassword } from "../utils/auth"
 
 export class AuthControler {
     static createAccount = async (req: Request, res: Response) => {
-        const { email } = req.body
+        const { email, password } = req.body
         
         const userExists = await User.findOne({where: { email }})
 
@@ -14,6 +15,7 @@ export class AuthControler {
 
         try {
             const user = new User(req.body)
+            user.password = await hashPassword(password)
             await user.save()
             res.json('Account created')
 
