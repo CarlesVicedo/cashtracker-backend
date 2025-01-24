6900a6df65b289b7430695f911c62f91f102f196
CarlesVicedo <vicedocarles@gmail.com>
2025-01-24 12:06:31 +0100

    Add logic for saving new users in DB

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,15 @@
 import type { Request, Response } from "express"
+import User from "../models/User"
 
 export class AuthControler {
     static createAccount = async (req: Request, res: Response) => {
-        res.json('Creating account...')
+        try {
+            const user = new User(req.body)
+            await user.save()
+            res.json('Account created')
+            
+        } catch (error) {
+            res.status(500).json({error: 'Something broke'})
+        }
     }
 }
