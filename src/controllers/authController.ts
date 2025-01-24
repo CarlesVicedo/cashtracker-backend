88c3070272f9e638f0730a12acd28cee25a55c8c
CarlesVicedo <vicedocarles@gmail.com>
2025-01-24 12:34:17 +0100

    Generate confirmation token and store it in the DB

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express"
 import User from "../models/User"
 import { hashPassword } from "../utils/auth"
+import { generateToken } from "../utils/token"
 
 export class AuthControler {
     static createAccount = async (req: Request, res: Response) => {
@@ -16,6 +17,7 @@ export class AuthControler {
         try {
             const user = new User(req.body)
             user.password = await hashPassword(password)
+            user.token = generateToken()
             await user.save()
             res.json('Account created')
 
